c139ee099dac196263096877f044fa6c0ca79708
Ali Alfredji <alfredjiali@gmail.com>
2021-05-11 19:37:04 +0200

    v 0.0.16

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ module.exports = {
     tiktokV1: require('./build/src/tiktok-v1'),
     instagramV1: require('./build/src/instagram-v1'),
     youtubeV1: require('./build/src/youtube-v1'),
+    youtubeV2: require('./build/src/youtube-v2'),
 }
